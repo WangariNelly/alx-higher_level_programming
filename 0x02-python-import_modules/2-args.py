99f4438f39d26fea3000a2eb819813711bb2ce33
LegacyNamari <nellywangari30@gmail.com>
2022-06-02 19:44:40 +0300

    Args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    """Prints the number of and the list of its arguments
-    Args:
-        system arguments
-    Returns:
-        There is no return value
-    """
-    import sys
-    no_of_arguments = len(sys.argv) - 1
-    if (no_of_arguments == 0):
-        print("0 arguments.")
-    elif (no_of_arguments == 1):
-        print("1 argument:")
-    else:
-        print("{:d} arguments:".format(no_of_arguments))
-    for argNo in range(no_of_arguments):
-        print("{}: {}".format(argNo + 1, sys.argv[argNo + 1]))
+
+    n = len(sys.argv)
+    s = "s" if n == 1 or n > 2 else ""
+    p = ":" if n > 1 else "."
+
+    print("{:d} argument{}{}".format(n - 1, s, p))
+
+    for i in range(1, n):
+        print("{:d}: {}".format(i, sys.argv[i]))
