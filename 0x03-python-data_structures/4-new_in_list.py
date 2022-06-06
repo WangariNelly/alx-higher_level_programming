e72cb2a86eef10b258adb42584fd6bebf2a8a482
LegacyNamari <nellywangari30@gmail.com>
2022-06-06 13:57:31 +0300

    Replace in

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,7 +6,7 @@ def new_in_list(my_list, idx, element):
     if ((idx < 0) or idx > (len(my_list) - 1)):
         return(my_list)
     else:
-        new_list = [num for num in my_list]
-        new_list[idx] = element
-        return(new_list)
+        new = [num for num in my_list]
+        new[idx] = element
+        return(new)
     
