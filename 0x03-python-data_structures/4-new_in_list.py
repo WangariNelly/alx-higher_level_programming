3559840374662c0bffee4cef09b43c16cea6ea69
LegacyNamari <nellywangari30@gmail.com>
2022-06-06 13:54:42 +0300

    Replace in

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+import numpy
+
+
 def new_in_list(my_list, idx, element):
     if ((idx < 0) or idx > (len(my_list) - 1)):
         return(my_list)
     else:
-        new_list = [i for num in my_list]
-        new[idx] = element
-        return(new)
+        new_list = [num for num in my_list]
+        new_list[idx] = element
+        return(new_list)
     
