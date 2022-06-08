d0a6516beea2af4721b21df724ba67aac350b933
LegacyNamari <nellywangari30@gmail.com>
2022-06-08 19:37:55 +0300

    diffrent

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     result = []
-    for element in set_1:
-        if element ^ set_2:
-            result.append(element)
+    for element in set_1 ^ set_2:
+        result.append(element)
     return result
- 
+     
