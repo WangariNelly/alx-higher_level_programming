db771e9d93d19e520e32e362809ce331d3fce895
LegacyNamari <nellywangari30@gmail.com>
2022-06-08 15:25:41 +0300

    unique

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,6 @@
 def uniq_add(my_list=[]):
-    result = sum(set(my_list))
-    return result
+    unique_list = []
+    for number in my_list:
+        if number not in unique_list:
+            unique_list.append(number)
+    return unique_list
