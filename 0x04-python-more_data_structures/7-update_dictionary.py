0deb3ae6d0a5888529cb76e06793371c8a969225
LegacyNamari <nellywangari30@gmail.com>
2022-06-08 20:11:04 +0300

    add key

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     new_key = []
-    if key == a_dictionary:
-        a_dictionary[new_key] = a_dictionary.pop(key)
-    if key ^ a_dictionary:
-        a_dictionary.append(new_key)
-    return new_key
+    for key in a_dictionary:
+        if key == a_dictionary:
+            a_dictionary[new_key] = a_dictionary.pop(key)
+        if key ^  a_dictionary:
+            a_dictionary.append(new_key)
+        return new_key
+
+
+        
