bdf4ad91b6119e74bfe5f4fa9178ec99ebd8b21b
LegacyNamari <nellywangari30@gmail.com>
2022-06-28 18:37:09 +0300

    Magic String

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(BestSchool):
-    for x in range(10):
-        return(magic_string(BestSchool))
+def magic_string(my_list=[]):
+    my_list.append("BestSchool")
+    return ','.join(my_list)
