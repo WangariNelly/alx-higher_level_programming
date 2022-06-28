566616f37469942fa0c3b0340203ab4cc1354067
LegacyNamari <nellywangari30@gmail.com>
2022-06-28 18:24:44 +0300

    Magic String

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    for x in range(5):
-        print(magic_string())
+def magic_string(BestSchool):
+    for x in range(10):
+        print(magic_string(BestSchool))
